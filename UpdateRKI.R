86e96411251ff1e389b2fab5a46b9aa4f91f57dc
Bojan <bogie@bawki.de>
2020-12-14 02:02:20 +0100

    forgot to load dplyr

--- UpdateRKI.R
+++ UpdateRKI.R
@@ -1,6 +1,7 @@
 download.file("https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data","rkiData/RKI_COVID19.csv")
 
 library(lubridate)
+library(dplyr)
 rkiData <- read.csv("rkiData/RKI_COVID19.csv",encoding = "UTF-8")
 
 rkiData$Meldedatum <- ymd_hms(rkiData$Meldedatum)
